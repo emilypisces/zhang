0bc6df72ad2d5ac549016e35efa00b69c291a975
emilypisces <100131635+emilypisces@users.noreply.github.com>
2022-02-21 20:19:17 +0800

    Create ex2.cpp

---
+++ ex2.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+
+    cout << "Hello Cindy ";
+    cin >> number;
+
+    cout << "You entered " << number;
+    return 0;
+}
