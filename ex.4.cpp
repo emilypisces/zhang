20d777e3d59d73f45e9398f9811b72665649b19b
emilypisces <100131635+emilypisces@users.noreply.github.com>
2022-03-07 19:11:01 +0800

    Create ex.4.cpp

---
+++ ex.4.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main (void) {
+     printf(" Hello Evon "); 
+     printf(" my height is %d m %d cm \n",1,80);
+     printf(" this building is %60d cm \n", 890);
+     printf(" this building is %60d cm \n", 7890);
+     printf(" this building is %60d cm \n", 67890);
+     printf(" this building is %60d cm \n", 567890);
+     printf(" this building is %60d cm \n", 1234567890);
+     printf(" pi = %1.13f \n ", 3.1415926535897932384626433832795);
+}
