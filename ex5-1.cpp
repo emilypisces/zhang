681de64f69500d0054d26641c069058a44103f0e
emilypisces <100131635+emilypisces@users.noreply.github.com>
2022-03-28 18:44:52 +0800

    Create ex5-1.cpp

---
+++ ex5-1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf(" Please input a \n ");
+    scanf(" %d" ,&a);
+    if(a > 9 )
+        printf("hello a > 9");
+    else
+        printf("hello world");
+    
+    return 0;
+}
