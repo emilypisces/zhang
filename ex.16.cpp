b1b7386926dd93449f9108606e6f8351ee5db5b2
emilypisces <100131635+emilypisces@users.noreply.github.com>
2022-04-25 18:30:30 +0800

    Create ex.16.cpp

---
+++ ex.16.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,a[3]={0};
+    
+    cin >> a[6];
+    for(i=0;i<3;i++){
+        cout << "a[" << i << "]=" << a[i] << endl;;
+    }
+    
+    cout<<"Hello World";
+
+    return 0;
+}
